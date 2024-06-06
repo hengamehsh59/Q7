d7f328079b751d6d11eead67956bfdf96a0fd698
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-06 18:15:28 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const float pi=3.14;
+    float r,s,a;
+    cin>>r;
+    s=2*pi*r;
+    a=pi*r*r;
+        cout<<"s:"<<s<<endl;
+        cout<<"a:"<<a<<endl;
+        return 0;
+    }
+     
